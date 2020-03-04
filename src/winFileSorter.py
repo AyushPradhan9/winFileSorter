56a6dab9b2b0873994eb323e1534e2dfdd4d6605
git-ayush-pradhan <ap.ayush.pradhn@gmail.com>
2020-03-04 20:29:31 +0530

    Update 0.1

--- src/winFileSorter.py
+++ src/winFileSorter.py
@@ -1,31 +1,32 @@
 import os
-import time
+import datetime
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+from tkinter import filedialog
 from tkinter import *
+from os import listdir
+from os.path import isfile, join
 
 win = Tk()
 win.title("File Sorter")
-win.geometry("500x500")
+win.geometry("680x600")
 win.configure(bg="Gray15")
-Label(win,text="Welcome to the File Sorter",font=("Times",17),height=5,bg="Gray15",fg="White").grid(row=0,column=1,padx=0,pady=10)
+current_path=os.getcwd()
 
-current_path = os.getcwd()
+Label(win,text="Welcome to the File Sorter",font=("Times",20),height=4,bg="Gray15",fg="White")\
+    .grid(row=0,column=0,padx=0,pady=0,sticky=N)
+Label(win,text="Select folder to be sorted :",font=("Times",12),width=20,height=1,bg="Gray15",fg="Red")\
+    .grid(row=1,column=0,padx=15,pady=10,sticky=W)
+label1=Label(win,text=current_path,font=("Times",10),width=65,height=1,bg="Gray23",fg="White")
+label1.grid(row=2,column=0,padx=0,pady=10)
 
-track = open("listFiles.txt", 'w+')
-reader = track.readlines(1)
-if not reader:
-    track.write("Sorted files are placed under the following folder:\n\n")
-    track.write("\tImage Files:\n\n")
-    track.write("\tVideo Files:\n\n")
-    track.write("\tDocument Files:\n\n")
-    track.write("\tCompressed Files:\n\n")
-    track.write("\tDatabase Files:\n\n")
-    track.write("\tExecutable Files:\n\n")
-    track.write("\tProgramming Files:\n\n")
-    track.write("\tSystem Files:\n\n")
-    track.write("\tOther Files:\n\n")
-track.close()
+def openDir():
+    global current_path
+    current_path=filedialog.askdirectory()
+    label1.config(text=current_path)
+
+Button(win,text='Browse',font=("Times",10),command=openDir,width=8,height=1,bg="Gray23",fg="White")\
+    .grid(row=2,column=2,padx=0,pady=10,sticky=E)
 
 extension = ''
 
@@ -39,13 +40,30 @@ program = ['.c', '.class', '.cpp', '.cs', '.h', '.java', '.swift', '.vb', '.sh']
 system = ['.bak', '.cab', '.cfg', '.cpl', '.cur', '.dll', '.dmp', '.ini', '.msi', '.sys', '.tmp', '.ini']
 
 def staticOne():
+    track = open(current_path + "/listFiles.txt", "w+")
+    reader = track.readlines(1)
 
-    file = [f for f in os.listdir(current_path) if os.path.isfile(f)]
+    if not reader:
+        track.write("Sorted files are placed under the following folder:\n\n")
+        track.write("\tImage Files:\n\n")
+        track.write("\tVideo Files:\n\n")
+        track.write("\tDocument Files:\n\n")
+        track.write("\tCompressed Files:\n\n")
+        track.write("\tDatabase Files:\n\n")
+        track.write("\tExecutable Files:\n\n")
+        track.write("\tProgramming Files:\n\n")
+        track.write("\tSystem Files:\n\n")
+        track.write("\tOther Files:\n\n")
+    track.close()
+    print(current_path)
+    file = [f for f in listdir(current_path) if isfile(join(current_path, f))]
+    print(file)
     for f in file:
+        currentDT = datetime.datetime.now()
         i=0
         name, ext = os.path.splitext(f)
 
-        track = open("listFiles.txt", 'r')
+        track = open(current_path + "/listFiles.txt", 'r')
         contents = track.readlines()
         track.close()
 
@@ -53,14 +71,14 @@ def staticOne():
             for x in contents:
                 i+=1
                 if x =='\tImage Files:\n':
-                    contents.insert(i,'\t\t'+f+'\n')
+                    contents.insert(i,'\t\t'+f+'\t\t'+"Sorted on - "+str(currentDT.strftime("%Y-%m-%d %H:%M:%S"))+'\n')
                 extension = '/Image Files'
 
         elif ext in videos:
             for x in contents:
                 i+=1
                 if x=='\tVideo Files:\n':
-                    contents.insert(i,'\t\t'+f+'\n')
+                    contents.insert(i,'\t\t'+f+'\t\t'+"Sorted on - "+str(currentDT.strftime("%Y-%m-%d %H:%M:%S"))+'\n')
                 extension = '/Video Files'
 
         elif ext in document:
@@ -70,21 +88,21 @@ def staticOne():
                 for x in contents:
                     i+=1
                     if x=='\tDocument Files:\n':
-                        contents.insert(i,'\t\t'+f+'\n')
+                        contents.insert(i,'\t\t'+f+'\t\t'+"Sorted on - "+str(currentDT.strftime("%Y-%m-%d %H:%M:%S"))+'\n')
                 extension = '/Document Files'
 
         elif ext in compressed:
             for x in contents:
                 i+=1
                 if x == '\tCompressed Files:\n':
-                    contents.insert(i,'\t\t'+f+'\n')
+                    contents.insert(i,'\t\t'+f+'\t\t'+"Sorted on - "+str(currentDT.strftime("%Y-%m-%d %H:%M:%S"))+'\n')
             extension = '/Compressed Files'
 
         elif ext in database:
             for x in contents:
                 i+=1
                 if x=='\tDatabase Files:\n':
-                    contents.insert(i,'\t\t'+f+'\n')
+                    contents.insert(i,'\t\t'+f+'\t\t'+"Sorted on - "+str(currentDT.strftime("%Y-%m-%d %H:%M:%S"))+'\n')
             extension = '/Database Files'
 
         elif ext in executable:
@@ -94,32 +112,32 @@ def staticOne():
                 for x in contents:
                     i+=1
                     if x == '\tExecutable Files:\n':
-                        contents.insert(i,'\t\t'+f+'\n')
+                        contents.insert(i,'\t\t'+f+'\t\t'+"Sorted on - "+str(currentDT.strftime("%Y-%m-%d %H:%M:%S"))+'\n')
                 extension = '/Executable Files'
 
         elif ext in program:
             for x in contents:
                 i+=1
                 if x=='\tProgramming Files:\n':
-                   contents.insert(i,'\t\t'+f+'\n')
+                   contents.insert(i,'\t\t'+f+'\t\t'+"Sorted on - "+str(currentDT.strftime("%Y-%m-%d %H:%M:%S"))+'\n')
             extension = '/Programming Files'
 
         elif ext in system:
             for x in contents:
                 i+=1
                 if x=='\tSystem Files:\n':
-                    contents.insert(i,'\t\t'+f+'\n')
+                    contents.insert(i,'\t\t'+f+'\t\t'+"Sorted on - "+str(currentDT.strftime("%Y-%m-%d %H:%M:%S"))+'\n')
             extension = '/System Files'
 
         else:
             for x in contents:
                 i+=1
                 if x=='\tOther Files:\n':
-                    contents.insert(i,'\t\t'+f+'\n')
+                    contents.insert(i,'\t\t'+f+'\t\t'+"Sorted on - "+str(currentDT.strftime("%Y-%m-%d %H:%M:%S"))+'\n')
             extension = '/Other Files'
 
         if i>0:
-            track = open("listFiles.txt", "r+")
+            track = open(current_path + "/listFiles.txt", "r+")
             track.seek(0)
             contents = "".join(contents)
             track.write(contents)
@@ -140,10 +158,16 @@ def dynamicOne():
     observe.schedule(event, current_path, recursive=True)
     observe.start()
 
-Button(win,text='Sort current files',font=("Times",10),command=staticOne,width=30,height=2,bg="Gray23",fg="White").grid(row=1,column=1,padx=0,pady=10)
-Button(win,text="Sort upcoming files",font=("Times",10),command=dynamicOne,width=30,height=2,bg="Gray23",fg="White").grid(row=2,column=1,padx=0,pady=20)
-Label(win,text="Note: Selecting sort upcoming files will require to leave app running",font=("Times",10),fg="Red",bg="Gray15").grid(row=3,column=1,padx=20,pady=20)
-Button(win,text="Exit",font=("Times",10),command=sys.exit,width=10,bg="Gray23",fg="White").grid(row=4,column=1,padx=0,pady=30)
+Label(win,text="Select type of sorting :",font=("Times",12),width=20,height=1,bg="Gray15",fg="Red")\
+    .grid(row=3,column=0,padx=0,pady=10,sticky=W)
+Button(win,text='Sort current files',font=("Times",11),command=staticOne,width=35,height=2,bg="Gray23",fg="White")\
+    .grid(row=4,column=0,padx=0,pady=10)
+Button(win,text="Cotinue sorting upcoming files",font=("Times",11),command=dynamicOne,width=35,height=2,bg="Gray23",fg="White")\
+    .grid(row=5,column=0,padx=0,pady=10)
+Label(win,text="Continue sorting upcoming files will require to leave app running",font=("Times",12),fg="Red",bg="Gray15")\
+    .grid(row=6,column=0,padx=0,pady=10,sticky=N)
+Button(win,text="Exit",font=("Times",10),command=sys.exit,width=10,bg="Gray23",fg="White")\
+    .grid(row=7,column=0,padx=0,pady=10)
 
 win.mainloop()
 
